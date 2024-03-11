015a1e54bbb904374ec9d7555b31823aa80d7ce4
Tsunami Noai <spam@falseblue.com>
2024-03-11 10:18:21 -0400

    Update hardware-configuration.nix to include /dev/sda as a boot device

--- hosts/ishtar/hardware-configuration.nix
+++ hosts/ishtar/hardware-configuration.nix
@@ -15,7 +15,7 @@
 
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
+    devices = ["/dev/sda"];
     efiSupport = true;
     efiInstallAsRemovable = true;
   };
