b4c296f9ad6e9cf7e6db721ecd1f4f5bf64b3e90
Tsunami Noai <spam@falseblue.com>
2024-03-11 11:32:35 -0400

    Update fonts configuration

--- home/tsunami/common/core/fonts.nix
+++ home/tsunami/common/core/fonts.nix
@@ -1,8 +1,42 @@
 {pkgs, ...}: {
-  fonts.fontconfig.enable = true;
-  home.packages = [
-    pkgs.noto-fonts
-    pkgs.nerdfonts # loads the complete collection. look into overide for FiraMono or potentially mononoki
-    pkgs.meslo-lgs-nf
-  ];
+  fonts = {
+    packages = with pkgs; [
+      material-icons
+      material-design-icons
+      roboto
+      work-sans
+      comic-neue
+      source-sans
+      twemoji-color-font
+      comfortaa
+      inter
+      lato
+      lexend
+      jost
+      dejavu_fonts
+      iosevka-bin
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      jetbrains-mono
+      (nerdfonts.override {fonts = ["DejaVuSansMono" "Iosevka" "JetBrainsMono"];})
+    ];
+
+    enableDefaultPackages = false;
+
+    # this fixes emoji stuff
+    fontconfig = {
+      defaultFonts = {
+        monospace = [
+          "Iosevka Term"
+          "Iosevka Term Nerd Font Complete Mono"
+          "Iosevka Nerd Font"
+          "Noto Color Emoji"
+        ];
+        sansSerif = ["Lexend" "Noto Color Emoji"];
+        serif = ["Noto Serif" "Noto Color Emoji"];
+        emoji = ["Noto Color Emoji"];
+      };
+    };
+  };
 }
