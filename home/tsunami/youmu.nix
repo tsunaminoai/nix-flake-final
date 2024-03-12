50b374adcc689988557b4c8e776f64f509eeb03a
Tsunami Noai <spam@falseblue.com>
2024-03-12 17:51:02 -0400

    Add neofetch, gh, alacritty configurations

--- home/tsunami/youmu.nix
+++ home/tsunami/youmu.nix
@@ -11,6 +11,9 @@ in {
     #################### Host-specific Optional Configs ####################
     common/optional/sops.nix
     common/optional/helper-scripts
+    common/optional/gh.nix
+    common/optional/alacritty.nix
+    common/optional/neofetch
   ];
   # Disable impermanence
   #home.persistence = lib.mkForce { };
