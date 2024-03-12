02e0310a1a5501400198863235f14ba206047f6f
Tsunami Noai <spam@falseblue.com>
2024-03-12 17:47:57 -0400

    Remove unused packages from default.nix

--- home/tsunami/common/core/default.nix
+++ home/tsunami/common/core/default.nix
@@ -62,13 +62,10 @@
       fzf # fuzzy search
       jq # JSON pretty printer and manipulator
       nix-tree # nix package tree viewer
-      ncdu # TUI disk usage
-      pciutils
       pfetch # system info
       pre-commit # git hooks
       p7zip # compresion & encryption
       ripgrep # better grep
-      usbutils
       tree # cli dir tree viewer
       unzip # zip extraction
       unrar # rar extraction
