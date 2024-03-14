981112454c0d5b1acad01b80a9373358b291057f
Tsunami Noai <spam@falseblue.com>
2024-03-14 17:36:09 -0400

    Update SSH configuration and move 'media' user to reference

--- home/bcraton/common/core/ssh.nix
+++ home/bcraton/common/core/ssh.nix
@@ -3,13 +3,6 @@
   pkgs,
   ...
 }: {
-  # Yubikey private key stubs
-  home.file.".ssh/id_rsa_yubikey.5cNFC.pub".text = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJxD/U4oHoCyZZfdPLNLEvdch0k2VKbuUJq5r8cKOPvs cardno:24_918_246
-  '';
-  home.file.".ssh/id_rsa_yubikey.nano5c.pub".text = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAN2yXKnrgQKJDHXqq6lE8v8Olm3theQk2JZ7yTMejwh cardno:23_183 _421
-  '';
   programs.ssh = {
     enable = true;
     forwardAgent = true;
@@ -22,24 +15,23 @@
         user = "git";
         hostname = "github.com";
         identityFile = [
+          "~/.ssh/id_yubikey"
           "~/.ssh/id_ed25519"
-          "~/.ssh/id_rsa_yubikey.5cNFC.pub"
-          "~/.ssh/id_rsa_yubikey.nano5c.pub"
           "~/.ssh/id_rsa"
         ];
       };
       "*.gensokyo" = {
         forwardAgent = true;
         identityFile = [
-          "~/.ssh/id_rsa_yubikey.5cNFC.pub"
-          "~/.ssh/id_rsa_yubikey.nano5c.pub"
+          "~/.ssh/id_yubikey"
+          "~/.ssh/id_ed25519"
         ];
       };
       "pways" = {
         hostname = "100.68.115.114";
         user = "bcraton";
         forwardAgent = false;
-        identityFile = "~/.ssh/id_rsa_yubikey.5cNFC.pub";
+        identityFile = "~/.ssh/id_yubikey";
         identitiesOnly = true;
       };
     };
--- hosts/common/optional/yubikey/scripts/yubikey-down.sh
+++ hosts/common/optional/yubikey/scripts/yubikey-down.sh
@@ -5,6 +5,6 @@ destination="$HOME/.ssh"
 
 echo "ran" > "${destination}/yubikey-down.log"
 #rm /etc/ssh/{id_yubikey,id_yubikey.pub}
-rm ${destination}/.ssh/{id_yubikey,id_yubikey.pub}
+rm "${destination}/.ssh/{id_yubikey,id_yubikey.pub}"
 
-echo "deleted" >> "${destination}/yubikey-down.log
+echo "deleted" >> "${destination}/yubikey-down.log"
