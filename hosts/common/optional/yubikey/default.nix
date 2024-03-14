4f378c048dc07a58f571a4fe37b5af1fb85129ee
Tsunami Noai <spam@falseblue.com>
2024-03-14 17:24:28 -0400

    Fix Yubikey configuration for ssh and u2f authentication

--- hosts/common/optional/yubikey/default.nix
+++ hosts/common/optional/yubikey/default.nix
@@ -36,48 +36,57 @@ in
         yubikey-manager # For ykman
       ];
 
-      # FIXME: Put this behind an option for yubikey ssh
-      # Create ssh files
+      services =
+        if pkgs.system == "x86_64-linux"
+        then {
+          # FIXME: Put this behind an option for yubikey ssh
+          # Create ssh files
 
-      # FIXME: Not sure if we need the wheel one. Also my idProduct gruop is 0407
-      # Yubikey 4/5 U2F+CCID
-      # SUBSYSTEM == "usb", ATTR{idVendor}=="1050", ENV{ID_SECURITY_TOKEN}="1", GROUP="wheel"
-      # We already have a yubikey rule that sets the ENV variable
+          # FIXME: Not sure if we need the wheel one. Also my idProduct gruop is 0407
+          # Yubikey 4/5 U2F+CCID
+          # SUBSYSTEM == "usb", ATTR{idVendor}=="1050", ENV{ID_SECURITY_TOKEN}="1", GROUP="wheel"
+          # We already have a yubikey rule that sets the ENV variable
 
-      services.udev.extraRules = ''
-        # Link/unlink ssh key on yubikey add/remove
-        SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="1050", RUN+="${lib.getBin yubikey-up}/bin/yubikey-up"
-        SUBSYSTEM=="input", ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", RUN+="${lib.getBin yubikey-down}/bin/yubikey-down"
-      '';
+          udev.extraRules = ''
+            # Link/unlink ssh key on yubikey add/remove
+            SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="1050", RUN+="${lib.getBin yubikey-up}/bin/yubikey-up"
+            SUBSYSTEM=="input", ACTION=="remove", ENV{ID_VENDOR_ID}=="1050", RUN+="${lib.getBin yubikey-down}/bin/yubikey-down"
+          '';
+          # Yubikey required services and config. See Dr. Duh NixOS config for
+          # reference
+          pcscd.enable = true; # smartcard service
 
-      # FIXME: Need to create symlinks to the sops-decrypted keys
-      #
+          udev.packages = [
+            yubikey-personalization
+          ];
+        }
+        else {};
 
-      # enable pam services to allow u2f auth for login and sudo
-      security.pam.services = {
-        login.u2fAuth = true;
-        sudo.u2fAuth = true;
-      };
+      security =
+        if pkgs.system == "x86_64-linux"
+        then {
+          # FIXME: Need to create symlinks to the sops-decrypted keys
+          #
 
-      # enable pam.u2f
-      # u2f_keys are extracted from secrets.yaml to default `~/.config/Yubico/u2f_keys` location via ../../core/sops.nix
+          # enable pam services to allow u2f auth for login and sudo
+          security.pam.services = {
+            login.u2fAuth = true;
+            sudo.u2fAuth = true;
+          };
 
-      #FIXME /etc/pam.d/sudo is being written but there is other stuff in there with higher order that may be interfering. Also doesn't seem that this will work over ssh either so may have to wait.
-      security.pam.u2f = {
-        enable = true;
-        control = "sufficient";
-        cue = true; # A reminder message will be displayed prompting user to use u2f device
+          # enable pam.u2f
+          # u2f_keys are extracted from secrets.yaml to default `~/.config/Yubico/u2f_keys` location via ../../core/sops.nix
 
-        # override defaults `pam://$HOSTNAME` so that they match the keys and work across hosts
-        origin = "pam://hostname";
-        appId = "pam://hostname";
-      };
+          #FIXME /etc/pam.d/sudo is being written but there is other stuff in there with higher order that may be interfering. Also doesn't seem that this will work over ssh either so may have to wait.
+          security.pam.u2f = {
+            enable = true;
+            control = "sufficient";
+            cue = true; # A reminder message will be displayed prompting user to use u2f device
 
-      # Yubikey required services and config. See Dr. Duh NixOS config for
-      # reference
-      services.pcscd.enable = true; # smartcard service
-
-      services.udev.packages = [
-        yubikey-personalization
-      ];
+            # override defaults `pam://$HOSTNAME` so that they match the keys and work across hosts
+            origin = "pam://hostname";
+            appId = "pam://hostname";
+          };
+        }
+        else {};
     }
