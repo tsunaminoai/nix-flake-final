09b0f399dc277240ca6b9b9d38db0e63bfe3d4f7
Tsunami Noai <spam@falseblue.com>
2024-03-18 16:54:23 -0400

    Update direnv and add alacritty to casks

--- home/tsunami/common/core/direnv.nix
+++ home/tsunami/common/core/direnv.nix
@@ -3,8 +3,11 @@
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
-    enableFishIntegration = true;
-    silent = true;
+    # enabled in fish by default. no need to enable it
+    config = {
+      disable_stdin = true;
+    };
+
     nix-direnv.enable = true; # better than native direnv nix functionality - https://github.com/nix-community/nix-direnv
   };
 }
