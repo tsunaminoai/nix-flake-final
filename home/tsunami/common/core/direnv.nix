24045b7e3a2ff65ddf41485b0ee67c7aa636bb53
Tsunami Noai <spam@falseblue.com>
2024-03-18 16:47:09 -0400

    Add Fish integration and enable silent mode in direnv.nix

--- home/tsunami/common/core/direnv.nix
+++ home/tsunami/common/core/direnv.nix
@@ -3,6 +3,8 @@
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+    enableFishIntegration = true;
+    silent = true;
     nix-direnv.enable = true; # better than native direnv nix functionality - https://github.com/nix-community/nix-direnv
   };
 }
