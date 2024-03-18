6368483492383159e9cd014072f9873c3fffcdca
Tsunami Noai <spam@falseblue.com>
2024-03-18 17:01:31 -0400

    Formatting

--- home/tsunami/common/optional/desktops/sway/default.nix
+++ home/tsunami/common/optional/desktops/sway/default.nix
@@ -1,10 +1,10 @@
-{pkgs , ... }: {
-    wayland.windowManager.sway = {
+{pkgs, ...}: {
+  wayland.windowManager.sway = {
     enable = true;
     config = rec {
       modifier = "Mod4";
       # Use kitty as default terminal
-      terminal = "alacritty"; 
+      terminal = "alacritty";
       startup = [
         {command = "exec sleep 5; systemctl --user start kanshi.service";}
         # Launch Firefox on start
