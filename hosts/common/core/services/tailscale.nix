59fa09063c06b4fe978ba0888561c6ca8145748e
Tsunami Noai <spam@falseblue.com>
2024-03-18 18:16:07 -0400

    Add Tailscale configuration and firewall settings***

--- hosts/common/core/services/tailscale.nix
+++ hosts/common/core/services/tailscale.nix
@@ -1,6 +1,22 @@
-{config, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   services.tailscale = {
     enable = true;
+    extraUpFlags = [
+      "--ssh"
+    ];
+    useRoutingFeatures = "client";
     authKeyFile = config.sops.secrets."tailscale/auth-key".path;
+  };
+  firewall = {
+    enable = true;
+    trustedInterfaces = ["tailscale0"];
+    allowedUDPPorts = [config.services.tailscale.port];
+    allowPing = false;
+    logReversePathDrops = true;
   };
 }
