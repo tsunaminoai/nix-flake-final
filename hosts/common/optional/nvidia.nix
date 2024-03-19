d71aa1b092333a9b7f2f669b572d7a7c0a576446
Tsunami Noai <spam@falseblue.com>
2024-03-19 10:38:25 -0400

    Update nvidia.nix with environment and hardware changes

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -1,19 +1,38 @@
 {pkgs, ...}: {
-  hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+  environment = {
+    variables = {
+      GBM_BACKEND = "nvidia-drm";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      LIBVA_DRIVER_NAME = "nvidia";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      NIXOS_OZONE_WL = "1"; # https://github.com/NixOS/nixpkgs/issues/224332#issuecomment-1528748548
     };
 
-    nvidia.modesetting.enable = true;
+    systemPackages = with pkgs; [
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-tools
+      nvtop
+    ];
+  };
+
+  hardware = {
+    nvidia = {
+      open = false;
+      powerManagement.enable = true;
+      modesetting.enable = true;
+    };
+    opengl = {
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+        nvidia-vaapi-driver
+      ];
+    };
   };
 
   boot.initrd.kernelModules = ["nvidia"];
   services.xserver.videoDrivers = ["nvidia"];
-
-  environment = {
-    systemPackages = with pkgs; [nvtop];
-    sessionVariables.NIXOS_OZONE_WL = "1"; # https://github.com/NixOS/nixpkgs/issues/224332#issuecomment-1528748548
-  };
 }
