fe4d1a708fdcec73ee8092dffc52479739f6a714
Tsunami Noai <spam@falseblue.com>
2024-03-19 09:55:44 -0400

    Update NVIDIA configuration and environment variables

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -1,47 +1,19 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
-  # Enable the X11 server, required for NVIDIA drivers
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-  boot.kernelParams = [
-    "nvidia-drm.modeset=1"
-  ];
-
-  environment.variables = {
-    ENABLE_VKBASALT = "1";
-    GBM_BACKEND = "nvidia-drm";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    LIBVA_DRIVER_NAME = "nvidia";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  };
-
-  environment.systemPackages = with pkgs; [
-    vulkan-loader
-    vulkan-validation-layers
-    vulkan-tools
-  ];
-
+{pkgs, ...}: {
   hardware = {
-    nvidia = {
-      open = false;
-      powerManagement.enable = false;
-      powerManagement.finegrained = false;
-      nvidiaSettings = true;
-      modesetting.enable = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-    pulseaudio.support32Bit = true;
     opengl = {
       enable = true;
       driSupport = true;
       driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        # vaapiVdpau
-        # libvdpau-va-gl
-      ];
     };
+
+    nvidia.modesetting.enable = true;
+  };
+
+  boot.initrd.kernelModules = ["nvidia"];
+  services.xserver.videoDrivers = ["nvidia"];
+
+  environment = {
+    systemPackages = with pkgs; [nvtop];
+    sessionVariables.NIXOS_OZONE_WL = "1"; # https://github.com/NixOS/nixpkgs/issues/224332#issuecomment-1528748548
   };
 }
