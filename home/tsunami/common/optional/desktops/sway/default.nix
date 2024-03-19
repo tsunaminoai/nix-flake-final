b40765b8472635874cc36984e45d01f059e4ffab
TsunamiNoAi <spam@falseblue.com>
2024-03-19 17:27:47 -0400

    Add new settings to swaylock

--- home/tsunami/common/optional/desktops/sway/default.nix
+++ home/tsunami/common/optional/desktops/sway/default.nix
@@ -12,13 +12,24 @@
   programs.swaylock = {
     enable = true;
     settings = {
+      clock = true;
       color = "808080";
       font-size = 24;
       indicator-idle-visible = false;
       indicator-radius = 100;
-      line-color = "ffffff";
+      indicator-thickness = 7;
+      effect-blur = "7x5";
+      effect-vignette = "0.5:0.5";
+      ring-color = "bb00cc";
+      key-hl-color = "880033";
+      line-color = "00000000"; #RGBA
+      inside-color = "00000088"; #RGBA
       show-failed-attempts = true;
+      separator-color = "00000000"; #RGBA
+      grace = 2;
+      fade-in = 0.2;
     };
+    package = pkgs.swaylock-effects;
   };
   services.swayidle = {
     enable = true;
