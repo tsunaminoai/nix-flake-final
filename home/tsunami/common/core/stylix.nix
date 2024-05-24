51e44230b03fa1d98fe09d3ad23e58b7117d05c5
Tsunami Noai <spam@falseblue.com>
2024-05-23 21:30:20 -0400

    Disable stylix due to conflict between HM and nixosmodules

---
+++ home/tsunami/common/core/stylix.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  # stylix = {
+  #   # image = pkgs.fetchurl {
+  #   #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+  #   #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+  #   # };
+  #   image = config.lib.stylix.pixel "base0A";
+  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+  #   polarity = "dark";
+  # };
+}
