8e2f358c552beb28ddfd61f64d38cd48c8d5f77d
Tsunami Noai <spam@falseblue.com>
2024-05-23 21:30:20 -0400

    Disable stylix due to conflict between HM and nixosmodules

--- home/tsunami/common/core/default.nix
+++ home/tsunami/common/core/default.nix
@@ -12,17 +12,8 @@
     else "/home";
 in {
   # import the theme
-#  _module.args.theme = import ./theme;
+  #  _module.args.theme = import ./theme;
 
-  stylix = {
-#    image = pkgs.fetchurl {
-#        url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-#        sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-#    };
-    image = config.lib.stylix.pixel "base0A";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    polarity = "dark";
-  }; 
   imports =
     [
       # Packages with custom configs go here
@@ -40,6 +31,7 @@ in {
       ./screen.nix # hopefully rarely needed but good to have if so
       ./ssh.nix # personal ssh configs
       ./starship.nix # prompt
+      ./stylix.nix # personal theme
       ./taskwarrior.nix # task manager
       ./tmux.nix # terminal multiplexer
       ./zoxide.nix # cd replacement
