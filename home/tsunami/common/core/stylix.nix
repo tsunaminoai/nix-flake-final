1427111685a28fe9af9c744fc5c381da66c3f79d
TsunamiNoAi <spam@falseblue.com>
2024-05-24 21:35:24 -0400

    Remove stylix for the time being

--- home/tsunami/common/core/stylix.nix
+++ home/tsunami/common/core/stylix.nix
@@ -1,15 +1,17 @@
 {
   pkgs,
   config,
+  lib,
   ...
 }: {
-  # stylix = {
-  #   # image = pkgs.fetchurl {
-  #   #   url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-  #   #   sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-  #   # };
+#   stylix = lib.mkIf (builtins.hasAttr "stylix" config.lib) {
+  stylix = {
+   image = pkgs.fetchurl {
+     url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+     sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+   };
   #   image = config.lib.stylix.pixel "base0A";
-  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-  #   polarity = "dark";
-  # };
+     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+     polarity = "dark";
+   } ;
 }
