2c5fcf11dfea628e7c4cd008a76db0ae2a370f93
Tsunami Noai <spam@falseblue.com>
2024-05-31 16:12:28 -0400

    finer configuration for starship

--- home/tsunami/common/core/starship.nix
+++ home/tsunami/common/core/starship.nix
@@ -2,22 +2,22 @@
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
-    enableTransience = true;
+    enableTransience = false;
     settings = {
       add_newline = false;
 
       format = lib.concatStrings [
         "[](fg:#115aa8)"
+        "$username"
         "$os"
         "$hostname"
-        "[](bg:#9A348E fg:#115aa8)"
-        "$username"
-        "[](bg:#DA627D fg:#9A348E)"
+        "[](bg:#DA627D fg:#115aa8)"
         "$directory"
         "[](fg:#DA627D bg:#FCA17D)"
         "$git_branch"
         "$git_status"
         "[](fg:#FCA17D bg:#86BBD8)"
+        "$direnv"
         "$c"
         "$elixir"
         "$elm"
@@ -35,40 +35,52 @@
         "$docker_context"
         "[](fg:#86BBD8 bg:#33658A)"
         "$time"
-        "[ ](fg:#33658A)"
+        "[](fg:#33658A)"
       ];
 
       username = {
         show_always = true;
-        style_user = "bg:#9A348E";
-        style_root = "bg:#9A348E";
-        format = "[ $user ]($style)";
+        style_user = "bg:#115aa8";
+        style_root = "bg:#115aa8";
+        format = "[$user]($style)";
         disabled = false;
       };
 
       hostname = {
         style = "bg:#115aa8";
-        ssh_only = true;
+        ssh_only = false;
         ssh_symbol = "󰢩 ";
-        format = "[$hostname ]($style)";
+        format = "[$hostname$ssh_symbol ]($style)";
       };
 
       os = {
         style = "bg:#115aa8";
         disabled = false;
+        format = "[$symbol]($style)";
         symbols = {
-          Macos = " ";
-          Debian = " ";
-          Ubuntu = " ";
-          NixOS = "❄️ ";
+          Macos = "";
+          Debian = "";
+          Ubuntu = "";
+          NixOS = "❄️";
         };
+      };
+      direnv = {
+        symbol = "";
+        style = "bg:#86BBD8 fg:#111111";
+        format = "[$symbol$loaded ]($style)";
+        disabled = false;
+        loaded_msg = "󰉋 ";
+        allowed_msg = "󰇵 ";
+        not_allowed_msg = " ";
+        denied_msg = "  ";
+        unloaded_msg = " ";
       };
 
       directory = {
         style = "bg:#DA627D";
-        truncation_length = 100;
-        truncate_to_repo = false;
-        format = "[ $path ]($style)";
+        fish_style_pwd_dir_length = 10;
+        truncate_to_repo = true;
+        format = "[$path ]($style)";
         truncation_symbol = "…/";
         read_only = "";
         home_symbol = "~";
@@ -83,8 +95,8 @@
 
       git_branch = {
         style = "bg:#FCA17D fg:#111111";
-        format = "[$symbol$branch]($style)";
-        symbol = " ";
+        format = "[$symbol$branch ]($style)";
+        symbol = "";
         disabled = false;
       };
 
@@ -99,14 +111,14 @@
         stashed = "✭";
         renamed = "➜";
         deleted = "✖";
-        format = "[$conflicted$staged$modified$untracked$stashed$renamed$deleted]($style)";
+        format = "[$conflicted$staged$modified$untracked$stashed$renamed$deleted ]($style)";
         style = "bg:#FCA17D fg:#111111";
         disabled = false;
       };
       nix_shell = {
-        symbol = "❄️ ";
+        symbol = "❄️";
         style = "bg:#86BBD8 fg:#111111";
-        format = "[ $symbol ($name) ]($style)";
+        format = "[$symbol($name) ]($style)";
         disabled = false;
       };
 
@@ -114,14 +126,14 @@
         disabled = false;
         time_format = "%R"; # Hour:Minute Format
         style = "bg:#33658A";
-        format = "[ $time ]($style)";
+        format = "[$time ]($style)";
       };
 
       zig = {
         symbol = "⚡️ ";
         style = "bg:#86BBD8 fg:#111111";
         version_format = "v\${raw}";
-        format = "[$symbol($version )]($style)";
+        format = "[$symbol($version) ]($style)";
       };
     };
   };
