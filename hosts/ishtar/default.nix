4cdfa5865b80c1676f7bf8ab80297b18c10b1b39
TsunamiNoAi <spam@falseblue.com>
2024-05-31 21:47:22 -0400

    Add security profile to ishtar

--- hosts/ishtar/default.nix
+++ hosts/ishtar/default.nix
@@ -23,6 +23,7 @@
     # ../common/optional/services/clamav.nix # depends on optional/msmtp.nix
     # ../common/optional/msmtp.nix #required for emailing clamav alerts
     ../common/optional/services/openssh.nix
+    ../common/security
 
     #################### Users to Create ####################
     ../common/users/tsunami
