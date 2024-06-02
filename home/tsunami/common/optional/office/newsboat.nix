86c2d7e265c89f7833ca15cbd687cbc4920018d9
TsunamiNoAi <spam@falseblue.com>
2024-06-02 00:15:10 -0400

    add newsboat and helix

---
+++ home/tsunami/common/optional/office/newsboat.nix
@@ -0,0 +1,34 @@
+{...}:{
+  programs.newsboat = {
+    enable = true;
+    autoReload = true;
+    reloadTime = 15;
+    urls = [
+      {
+        url = "https://cybereason.com/blog/rss.xml";
+        tags = ["ciso"];
+        title = "Cybereason";
+      }
+       {
+        url = "https://cisomag.com/feed";
+        tags = ["ciso"];
+        title = "CISO Mag";
+      }
+ {
+        url = "https://cisostreet.com/feed";
+        tags = ["ciso"];
+        title = "CISO Street";
+      }
+ {
+        url = "https://welivesecurity.com/feed";
+        tags = ["security"];
+        title = "WeLiveSecurity";
+      }
+
+    ];
+  };
+}
+
+
+
+
