863e28e9d28cae01ba2884bdb8843bfefd105cdf
TsunamiNoAi <spam@falseblue.com>
2024-06-05 17:24:19 -0400

    remove line

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     #################### Official NixOS Package Sources ####################
 
-    # nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.05"; 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable"; # also see 'unstable-packages' overlay at 'overlays/default.nix"
 
