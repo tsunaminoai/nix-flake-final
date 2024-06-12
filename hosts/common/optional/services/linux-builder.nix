38df5d726108c1a179b0afd6f06f6cb2315e707a
TsunamiNoAi <spam@falseblue.com>
2024-06-11 20:58:11 -0400

    add mkDefault to builders

--- hosts/common/optional/services/linux-builder.nix
+++ hosts/common/optional/services/linux-builder.nix
@@ -1,11 +1,11 @@
-{...}:{
+{lib, ...}:{
   nix = {
     settings = {
-      builders = [
+      builders = lib.mkDefault [
         "ssh://builder@ishtar aarch64-linux x86_64-linux"
         "ssh://builder@mokou aarch64-linux x86_64-linux"
       ];
     };
-  }
+  };
   
 }
