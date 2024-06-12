573c1a4d3f58f0f11041070297ba7a7198fac29d
TsunamiNoAi <spam@falseblue.com>
2024-06-11 20:41:43 -0400

    adjust boostrap to comply with flake and add helper scripts

--- hosts/bootstrap/default.nix
+++ hosts/bootstrap/default.nix
@@ -34,43 +34,33 @@
   # Root password and SSH keys. If network config is incorrect, use this password
   # to manually adjust network config on serial console/VNC.
   users.mutableUsers = false;
-  users.users.root = {
-    hashedPassword = "$6$9iybgF./X/RNsRrQ$h7Zlk//loJDPg7yCCPT/9jVU0Tvep6vEA1FvPBT.kqJUA5qlzhDJEYnBFlpBZmTXuUXjF0qgmDWmGkXIMC9JD/";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcWoEQ4Mh27AV3ixcn9CMaUK/R+y4y5TqHmn2wJoN6i lantian@lantian-lenovo-archlinux"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCulLscvKjEeroKdPE207W10MbZ3+ZYzWn34EnVeIG0GzfZ3zkjQJVfXFahu97P68Tw++N6zIk7htGic9SouQuAH8+8kzTB8/55Yjwp7W3bmqL7heTmznRmKehtKg6RVgcpvFfciyxQXV/bzOkyO+xKdmEw+fs92JLUFjd/rbUfVnhJKmrfnohdvKBfgA27szHOzLlESeOJf3PuXV7BLge1B+cO8TJMJXv8iG8P5Uu8UCr857HnfDyrJS82K541Scph3j+NXFBcELb2JSZcWeNJRVacIH3RzgLvp5NuWPBCt6KET1CCJZLsrcajyonkA5TqNhzumIYtUimEnAPoH51hoUD1BaL4wh2DRxqCWOoXn0HMrRmwx65nvWae6+C/7l1rFkWLBir4ABQiKoUb/MrNvoXb+Qw/ZRo6hVCL5rvlvFd35UF0/9wNu1nzZRSs9os2WLBMt00A4qgaU2/ux7G6KApb7shz1TXxkN1k+/EKkxPj/sQuXNvO6Bfxww1xEWFywMNZ8nswpSq/4Ml6nniS2OpkZVM2SQV1q/VdLEKYPrObtp2NgneQ4lzHmAa5MGnUCckES+qOrXFZAcpI126nv1uDXqA2aytN6WHGfN50K05MZ+jA8OM9CWFWIcglnT+rr3l+TI/FLAjE13t6fMTYlBH0C8q+RnQDiIncNwyidQ== lantian@LandeMacBook-Pro.local"
-    ];
-  };
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH2IrKNs2BLgup7rSVt7KJqRqeSxhU+B1FUrBlHNNmSJ tsunami@youmu"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBm4yzmOrF+MCV+w0yfd10R88iHR6QusZBCpEtPFm+f+ tsunami@mokou"
+  ];
+
 
   # Manage networking with systemd-networkd
   systemd.network.enable = true;
   services.resolved.enable = false;
 
   # Configure network IP and DNS
-  systemd.network.networks.eth0 = {
-    address = ["123.45.678.90/24"];
-    gateway = ["123.45.678.1"];
-    matchConfig.Name = "eth0";
-  };
-  networking.nameservers = [
+  networking = {
+useDHCP = lib.mkDefault true;
+  nameservers = [
     "8.8.8.8"
   ];
-
+  firewall.enable = false;
+  };
   # Enable SSH server and listen on port 2222
   services.openssh = {
     enable = true;
-    ports = [2222];
+    ports = [22];
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = lib.mkForce "prohibit-password";
     };
   };
-
-  # Disable NixOS's builtin firewall
-  networking.firewall.enable = false;
-
-  # Disable DHCP and configure IP manually
-  networking.useDHCP = false;
 
   # Hostname, can be set as you wish
   networking.hostName = "bootstrap";
