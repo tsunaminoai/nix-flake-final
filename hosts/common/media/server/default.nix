1f55216d2ff046803290eb308e19502b35f7755a
TsunamiNoAi <spam@falseblue.com>
2024-06-19 18:19:33 -0400

    Expiriment with backing up the cache

--- hosts/common/media/server/default.nix
+++ hosts/common/media/server/default.nix
@@ -1,4 +1,4 @@
-{
+rec {
   imports = [
     ./smbclient.nix
     ./kavita.nix
@@ -16,4 +16,26 @@
     openFirewall = true;
     host = "0.0.0.0";
   };
+
+  #TODO: Definitely cool that this works, but not viable without global backup strat
+  
+  # services.borgmatic = {
+  #   enable = true;
+  #   configurations = {
+  #     media-metadata = {
+  #       source_directories = [
+  #         "/var/lib/jellyfin"
+  #         "/var/lib/audiobookshelf"
+  #         "/var/lib/kavita"
+  #       ];
+  #       repositories = [
+  #         {
+  #           path = "ssh://borgwarehouse@voile.gensokyo:22222/./7e4cca68";
+  #           label = "BorgWareHouse@Voile";
+  #         }
+  #       ];
+  #           keep_daily = 7;
+  #     };
+  #   };
+  # };
 }
