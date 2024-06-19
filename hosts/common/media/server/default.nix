1c2e60bddf8625c1336f027b0550f750e245ef21
TsunamiNoAi <spam@falseblue.com>
2024-06-19 16:55:16 -0400

    Add a homepage

--- hosts/common/media/server/default.nix
+++ hosts/common/media/server/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./smbclient.nix
     ./kavita.nix
+    ./homepage.nix
   ];
   # the config diretory by default is /var/lib/jellyfin/config
   #todo: should this be simply backedup or could we do XML/DB generation from here
@@ -9,4 +10,11 @@
     enable = true;
     openFirewall = true;
   };
+
+  services.audiobookshelf = {
+    enable = true;
+    openFirewall = true;
+    host = "0.0.0.0";
+  };
+  
 }
