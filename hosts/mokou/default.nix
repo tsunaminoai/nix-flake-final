4846aed100eea4c3f5ec59994746f2329140e9b8
TsunamiNoAi <spam@falseblue.com>
2024-06-19 18:26:03 -0400

    Add media files from the server branch

--- hosts/mokou/default.nix
+++ hosts/mokou/default.nix
@@ -32,6 +32,9 @@
 
     #################### Users to Create ####################
     ../common/users/tsunami
+
+    ### Modules to Test ###
+    ../common/media/server
   ];
   # set custom autologin options. see greetd.nix for details
 
