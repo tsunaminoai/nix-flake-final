8e4cb2237f22954e67ce9e01db9de2078b887a5d
Tsunami Noai <spam@falseblue.com>
2024-07-08 15:00:23 -0400

    Added desktop WIP and fonts modules.

---
+++ modules/nixos/desktop/default.nix
@@ -0,0 +1,82 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+with lib; let
+  cfg = config.tsunaminoai.desktop;
+in {
+  options.tsunaminoai.desktop = with types; {
+    enable = mkBoolOpt false "Enable desktop configuration";
+    windowManager = lib.mkOption {
+      type = types.enum ["sway"];
+      default = "sway";
+      description = ''
+        The window manager to use.
+      '';
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      tsunaminoai.fonts.enable = true;
+
+      services = {
+        pipewire = {
+          enable = true;
+          alsa.enable = true;
+          alsa.support32Bit = true;
+          pulse.enable = true;
+        };
+      };
+      # the languagetool extension for code needs a server running
+      languagetool.enable = true;
+    })
+
+    (lib.mkIf (cfg.enable && cfg.windowManager == "sway") {
+      programs = {
+        dconf.enable = true;
+        noisetorch.enable = true;
+      };
+      services = {
+        greetd = {
+          enable = true;
+          settings = rec {
+            initial_session = default_session;
+            default_session = {
+              command = let
+                timeFormat = "%Y-%m-%d | %H:%M:%S";
+                greeting = "Welcome! Access is restricted.";
+              in "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '${timeFormat}' --greeting '${greeting}' --cmd sway";
+              user = "greeter";
+            };
+          };
+        };
+      };
+
+      xdg.portal = {
+        enable = true;
+        # there is some weirdness happening here
+        # https://github.com/NixOS/nixpkgs/issues/160923
+        #xdgOpenUsePortal = true;
+        wlr.enable = true;
+        extraPortals = [pkgs.xdg-desktop-portal-gtk];
+        config.common.default = ["gtk"];
+      };
+      security = {
+        polkit.enable = true;
+
+        # this is necessary for PipeWire
+        rtkit.enable = true;
+
+        /*
+        this is required for swaylock to be able to verify credentials
+        without this swaylock will always complain about a wrong password
+        */
+        pam.services.swaylock = {};
+      };
+    })
+  ];
+}
